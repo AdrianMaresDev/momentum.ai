41ec14908ccc9d35cf981dafab874e2b010d6d73
AdrianMares <adrianmares.dev@gmail.com>
2025-01-12 23:33:56 -0500

    building user login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -38,6 +38,32 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
     //The passwords are encrypted in the database, so the password that the user enters must also be encrypted
     //When the two encrypted passwords are compared, they will match
+
+    //Destructure the username and password
+    const { username, password } = req.body;
+
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?');
+        const user = getUser.get(username);
+
+        //If no user is found, return out of the function
+        if(!user) { return res.status(404).send({ message: 'User not found' }) };
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+        //If the password does not match, return out of the function
+        if(!passwordIsValid) { return res.status(404).send({ message: 'Invalid password' }) };
+
+        console.log(user);
+        //Upon successful authentication
+        const token = jwt.sign({ id: user.id }, 
+            process.env.JWT_SECRET, 
+            { expiresIn: '24h' });
+        res.json({ token });
+    } catch(err) {
+        console.log(err.message);
+        res.sendStatus(503);
+    }
 });
 
 export default router;
