9de63cb391ca514f933e4995ccfc1fa29f9bcc82
AdrianMares <adrianmares.dev@gmail.com>
2025-01-12 23:47:52 -0500

    middleware

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -4,7 +4,11 @@ import db from '../db.js';
 const router = express.Router();
 
 //Get all tasks for logged in user
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+    const getTasks = db.prepare('SELECT * FROM tasks WHERE user_id = ?');
+    const tasks = getTasks.all(req.userId);
+    res.json({ tasks })
+});
 
 //Create new task
 router.post('/', (req, res) => {});
